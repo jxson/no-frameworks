21dddcefd2170cb4699a4fe5402b5cf7c1aea553
Jason Campbell <jason@js.la>
2013-07-16 12:49:32 -0700

    Adds json support

---
+++ test/test-post-recipes.js
@@ -0,0 +1,20 @@
+var request = require('supertest')
+  , server = require('../lib/server')
+  , assert = require('assert')
+
+describe('POST /recipes', function(){
+  before(function(){
+    server.logger.level('fatal')
+  })
+
+  it('successfully handles json', function(done){
+    request(server)
+    .post('/recipes')
+    .send({ title: 'Tamales' })
+    .type('json')
+    .expect('content-type', /json/)
+    .expect(201, done)
+  })
+
+  it('successfully handles form data')
+})
